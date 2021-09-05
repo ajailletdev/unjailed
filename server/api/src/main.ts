961ed5191b7f6583ec72d1c50dda6a828dc22f99
ajailletdev <adrien.jaillet.dev@gmail.com>
2021-09-05 20:26:38 +0200

    fix

--- server/api/src/main.ts
+++ server/api/src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cors from 'cors';
 import * as helmet from 'helmet';
-import * as history from 'connect-history-api-fallback';
  
 //var csurf = require('csurf');
 
@@ -11,7 +10,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cors());
   app.use(helmet());
-  app.use(history());
   // app.use(csurf());
 
   await app.listen(3000);
